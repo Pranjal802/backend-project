3cf87ff4d5609d1ec87c5a4bc255385b907907fb
Pranjal <patelpranjal802@gmail.com>
2025-04-23 13:23:03 +0530

    Updation code for avatar and coverImage and made subscription mode

---
+++ src/models/subscription.model.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId, // One who is subscribing
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId, // The channel being subscribed to
+      ref: "User", // Assuming a user is a channel
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
